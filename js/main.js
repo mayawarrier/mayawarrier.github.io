1e49da21eb6a46627c345b27d6d229fb9d9a832e
Dhruv Warrier <dhruv.warrier89@gmail.com>
2018-08-09 16:51:06 +0400

    Completed fade in animation of text panels - Panels scroll into view and fade in one by one - Scrolling uses the smoothscroll library - To be done: Fading in the resume link when in viewport

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-// JavaScript source code
+
+var panelIndex = 1;
+var panelsShowing = false;
+
+function togglePanels()
+{
+    if (!panelsShowing) {
+        panelsShowing = true;
+        $(".textPanel").css({ "opacity": "0.01", "display": "block" });
+        document.querySelector("#panel2").scrollIntoView({ behavior: 'smooth' });
+        animateFadeIn();
+    }
+}
+
+function animateFadeIn() {
+
+    var timeBetweenFadeIns = 300;
+    var timeToFadeIn = 400;
+
+    setTimeout(() => {
+        $("#panel" + panelIndex).animate({ "opacity": "1" }, timeToFadeIn, () => {
+            panelIndex++;
+            animateFadeIn();
+        });
+    }, timeBetweenFadeIns);
+}
