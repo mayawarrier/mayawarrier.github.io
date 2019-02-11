1d553a5ea02c02fd67152e934a2f00ec408ba773
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-02-10 22:25:02 -0500

    Remove vertical centering when panels link is clicked - Unset position and transform: none main-container so that the main-container content gets pushed up by the panels

--- js/main.js
+++ js/main.js
@@ -14,9 +14,15 @@ function togglePanels()
 {
     if (!panelsShowing) {
         panelsShowing = true;
+        // remove vertical centering so that the panels scroll into view normally
+        $("#main-container").css({ "position": "unset", "transform": "none" });
+        // remove display: none from panels so that we scroll to the center while the panels fade into view
         $(".text-panel").css({ "opacity": "0.01", "display": "block" });
+        // scroll to the center of the panels
         document.querySelector("#panel2").scrollIntoView({ behavior: 'smooth' });
+        // fade in the panels
         animatePanelsFadeIn();
+        // remove display: none from the resume link so it can fade in when in viewport
         $("#resume").css({ "opacity": "0.01", "display": "block" });
     }
 }
