db4bb09b8256f39bd3fb7fecec379e40ad74b4ac
Maya Warrier <maya.warrierm@gmail.com>
2025-11-30 17:35:01 -0500

    Fix description width on small screens

--- src/components/project-tile.tsx
+++ src/components/project-tile.tsx
@@ -20,6 +20,8 @@ export interface ProjectTileFC extends React.FC<ProjectTileProps> {
   }>;
   Desc: React.FC<{
     text: string;
+    /** Use dangerouslySetInnerHTML */
+    renderHTML?: boolean;
   }>;
 }
 
@@ -39,41 +41,41 @@ export const ProjectTile: ProjectTileFC = (props) => {
               className="w-full h-48 object-cover object-center rounded-md" />
           )}
 
-          <div className="flex flex-row gap-2 justify-between p-1">
-            {/* text */}
-            <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4">
+            {/* title + links */}
+            <div className="flex flex-row gap-2 justify-between p-1">
               <h3 className="text-lg lg:text-xl font-medium 
-                  text-foreground group-hover:text-primary transition-colors">
+                text-foreground group-hover:text-primary transition-colors">
                 {props.title}
               </h3>
-              <div className="space-y-4 max-w-3xl xl:max-w-208">
-                {props.children}
-              </div>
-            </div>
-
-            {/* links */}
-            <div className="flex flex-row items-start gap-2 text-sm">
-              {props.extLinks.map((obj, idx) => {
-                if (!obj.url) {
-                  return null;
-                }
-                const Icon = obj.icon;
-                return (
-                  <a
-                    key={idx}
-                    href={obj.url}
-                    target="_blank"
-                    onClick={(e) => e.stopPropagation()}
-                    className="flex flex-row items-center justify-center gap-1 py-1 px-2
+              <div className="flex flex-row items-start gap-2 text-sm">
+                {props.extLinks.map((obj, idx) => {
+                  if (!obj.url) {
+                    return null;
+                  }
+                  const Icon = obj.icon;
+                  return (
+                    <a
+                      key={idx}
+                      href={obj.url}
+                      target="_blank"
+                      onClick={(e) => e.stopPropagation()}
+                      className="flex flex-row items-center justify-center gap-1 py-1 px-2
                         text-muted-foreground border-muted-foreground/30 border-1 rounded-md 
                         hover:text-accent hover:border-accent transition-colors">
-                    <Icon className="h-4 w-4" />
-                    <span className="hidden lg:block">{obj.label}</span>
-                  </a>
-                );
-              })}
+                      <Icon className="h-4 w-4" />
+                      <span className="hidden lg:block">{obj.label}</span>
+                    </a>
+                  );
+                })}
+              </div>
+            </div>
+            {/* description */}
+            <div className="space-y-4 max-w-3xl xl:max-w-208 px-1">
+              {props.children}
             </div>
           </div>
+
         </div>
 
         {/* divider */}
@@ -93,13 +95,11 @@ ProjectTile.DescList = (props) => {
       </div>
       <ul className="list-disc pl-6 text-muted-foreground/80 [&_a]:underline [&_a]:text-accent
         group-hover:text-foreground/60 group-hover:[&_a]:text-primary/80 transition-colors">
-        {props.items.map((desc, descIdx) => {
-          if (props.renderHTML === true) {
-            return <li key={descIdx} dangerouslySetInnerHTML={{ __html: desc }} />;
-          } else {
-            return <li key={descIdx}>{desc}</li>;
-          }
-        })}
+        {props.items.map((desc, descIdx) => (
+          props.renderHTML === true ?
+            <li key={descIdx} dangerouslySetInnerHTML={{ __html: desc }} /> :
+            <li key={descIdx}>{desc}</li>
+        ))}
       </ul>
     </div>
   );
@@ -109,7 +109,10 @@ ProjectTile.Desc = (props) => {
   return (
     <p className={`text-muted-foreground/80 [&_a]:underline [&_a]:text-accent
         group-hover:text-foreground/60 group-hover:[&_a]:text-primary/80 transition-colors`}>
-      {props.text}
+      {props.renderHTML === true ?
+        <span dangerouslySetInnerHTML={{ __html: props.text }} /> :
+        props.text
+      }
     </p>
   );
 };
