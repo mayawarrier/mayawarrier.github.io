5daa687730d8a3f24c965897087f23cf6c3ed4a3
Maya Warrier <maya.warrierm@gmail.com>
2025-12-30 23:59:29 +0100

    Use svh instead of dvh for screen height

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,11 +5,11 @@ import { Showcase } from "~/components/showcase";
 export default function Home() {
   const showcaseRef = useRef<HTMLDivElement>(null);
   return (
-    <main className="min-h-dvh flex flex-col lg:flex-row lg:h-dvh">
-      <div className="h-dvh lg:w-2/5 lg:overflow-y-hidden">
+    <main className="min-h-svh flex flex-col lg:flex-row lg:h-svh">
+      <div className="h-svh lg:w-2/5 lg:overflow-y-hidden">
         <Hero showcaseRef={showcaseRef}/>
       </div>
-      <div className="min-h-dvh h-auto w-full lg:block lg:h-dvh lg:w-3/5 lg:h-full" >
+      <div className="min-h-svh h-auto w-full lg:block lg:h-svh lg:w-3/5 lg:h-full" >
         <Showcase ref={showcaseRef} />
       </div>
     </main>
